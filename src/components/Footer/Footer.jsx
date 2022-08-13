9bdd1297bf84ef7ff104cb2f9cf81fb4f8b6657c
aslanshaken <aslanshaken@gmail.com>
2022-08-13 16:25:33 -0500

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,58 @@
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './Footer.css';
+import Logo from '../../assets/logo.png';
+import Twitter from '../../assets/twitter.png';
+import Facebook from '../../assets/facebook.png';
+import Instagram from '../../assets/instagram.png';
+import Phone from '../../assets/phone.png';
 
 
 export default function Footer() {
   return (
-      <div>
-          <h1>Footer</h1>
+    <div className='footer-container'>
+      <div className='footer-container-first'>
+        <img alt="logo" src={Logo} className="fotter-logo-img" />
+        <p>Financial solutions and online accounting excellence </p>
       </div>
-  )
+      <div className='footer-container-second'>
+        <div className='footer-row'>
+          <p className='g-txt'>Our Company</p>
+          <a href="/">Home</a>
+          <a href="/services">Services</a>
+          <a href="/contact">Contact</a>
+          <a href="/about">About us</a>
+        </div>
+        <div className='footer-row'>
+          <p className='g-txt'>Our Services</p>
+          <a href="/">Bookkeeping </a>
+          <a href="/">Payroll</a>
+          <a href="/">Tax</a>   
+        </div>
+        <div className='footer-row'>
+          <p className='g-txt'>Cases</p>
+          <a href="/">Small business</a>
+          <a href="/">Medium business</a>
+          <a href="/">Large business</a>
+        </div>
+        {/* <div>
+          <p>Our Offices</p>
+          <p>1 Liberty St #226,<br/>
+          New York, NY 10006
+          </p>
+          </div> */}
+      </div>
+      <div className='footer-container-third'>
+        <div className='fotter-container-icons'>
+          <a href='/'><img alt="icons" src={Twitter}/></a>
+          <a href='/'><img alt="icons" src={Facebook}/></a>
+          <a href='/'><img alt="icons" src={Instagram}/></a>
+        </div>
+        <a href="tel:917-200-7609">+1-917-200-7609</a>
+        <a href="mailto:go4profit@gmail.com">go4profit@gmail.com</a>
+        {/* <div>Ⓒ Go4profit 2022 All rights are reserved </div> */}
+      </div>
+    </div>
+  );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import { Navbar, Nav, Container, Button, Image } from "react-bootstrap";
 import React from "react";
-import { Avatar } from '@mui/material';
+import {useLocation} from 'react-router-dom';
 import './Header.css'
 import Logo from '../../assets/logo.png'
 
 
 function Header() {
+  const location = useLocation();
+  const path = location.pathname
+
   return (
     <Navbar expand="xl" sticky="top" className="gap-3 pt-5 pb-5" bg="light" variant="light">
       <Container className="align-items:center color-black">
@@ -15,11 +18,11 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="flex-grow-1 justify-content-center align-items-center">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About us</Nav.Link>
-            <Nav.Link href="/why-us">Why us</Nav.Link>
-            <Nav.Link href="/service">Services</Nav.Link>
-            <Nav.Link href="/join-us">Join us</Nav.Link>
+            <Nav.Link href="/" className={path === '/' && 'active'} >Home</Nav.Link>
+            <Nav.Link href="/about" className={path === '/about' && 'active'}>About us</Nav.Link>
+            <Nav.Link href="/why-us" className={path === '/why-us' && 'active'}>Why us</Nav.Link>
+            <Nav.Link href="/services" className={path === '/services' && 'active'}>Services</Nav.Link>
+            <Nav.Link href="/join-us" className={path === '/join-us' && 'active'}>Join us</Nav.Link>
             <Nav.Link className="mv-button-right">
               <Button className="call-button">Request a call</Button>
             </Nav.Link>
