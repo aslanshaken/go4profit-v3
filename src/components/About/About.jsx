3b7bdffe34d0323b572bbd50325d8a9ebfd54df4
aslanshaken <aslanshaken@gmail.com>
2022-08-27 17:42:41 -0500

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,6 +2,14 @@ import './About.css';
 import Button from 'react-bootstrap/Button';
 import Eclipse from '../../assets/ecliplse.png';
 import FirstImage from '../../assets/about1.png';
+import Vector from '../../assets/Vector.png';
+import Vector1 from '../../assets/Vector-1.png';
+import Vector7 from '../../assets/Vector7.png';
+import Vector3 from '../../assets/Vector-3.png';
+import Vector4 from '../../assets/Vector-4.png';
+import Vector5 from '../../assets/Vector-5.png';
+import Finance from '../../assets/finance-tutor-4.png';
+
 
 export default function About() {
   return(
@@ -33,7 +41,8 @@ export default function About() {
       <div className='about-second'>
         <div className='about-second-a'>
         <h3>Why us ?</h3>
-        <p>Skilled bookkeeping experts</p>
+        <h4>Skilled bookkeeping experts</h4>
+        <br/>
         <p>It’s hard enough to find a good bookkeeper, 
           let alone ones who understand your product and 
           market. We bring qualified bookkeeping experts 
@@ -46,6 +55,56 @@ export default function About() {
         </div>
         <img alt='ima' src={FirstImage}/>
       </div>
+      <div className='about-third'>
+        <div>
+        <h2>Industry and technical expertise</h2>
+        <p>Deep expertise in the bookkeeping technology market</p>
+        </div>
+        <div className='about-third-c'>
+          <div className='about-third-c-card'>
+            <img alt='vector' src={Vector} />
+            <p>Growth</p>
+          </div>
+          <div className='about-third-c-card'>
+            <img alt='vector' src={Vector1} />
+            <p>Personal</p>
+          </div>
+          <div className='about-third-c-card'>
+            <img alt='vector' src={Vector3} />
+            <p>Safety</p>
+          </div>
+          <div className='about-third-c-card'>
+            <img alt='vector' src={Vector4} />
+            <p>Dedicated team</p>
+          </div>
+          <div className='about-third-c-card'>
+            <img alt='vector' src={Vector5} />
+            <p>Cost-effective</p>
+          </div>
+          <div className='about-third-c-card'>
+            <img alt='vector' src={Vector7} />
+            <p>Hardware</p>
+          </div>
+        </div>
+      </div>
+      <div className='about-forth'>
+        <img alt='ima' src={Finance}/>
+        <div className='about-forth-a'>
+        <h3>Focused on growth</h3>
+        <br/>
+        <p>We don’t believe in ad hoc marketing that can’t 
+          deliver real results. Our approach to every project 
+          is based on your big goals and milestones, from an 
+          upcoming product launch to meeting investor 
+          expectations before your next round of funding.
+           If you don’t yet have the tools to measure the 
+           success of your marketing campaigns, we’ll help 
+           you put them in place.
+        </p>
+        </div>
+        <p className='about-forth-overflow-txt'>LEADERSHIP</p>
+      </div>
+      <div className='about-forth-space'></div>
     </div>
   )
 }
