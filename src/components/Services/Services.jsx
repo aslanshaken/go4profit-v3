21ac69b76f301c2735924b7330aecc4f27b84205
aslanshaken <aslanshaken@gmail.com>
2022-09-05 21:45:55 -0500

    services added items

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,11 +1,18 @@
 import './Services.css';
 import Button from 'react-bootstrap/Button';
 import Checkbox from '../../assets/green-checkbox.png';
+import Stripe from '../../assets/stripe.png';
+import Gusto1 from '../../assets/gusto1.png';
+import Shopify1 from '../../assets/shopify1.png';
+import BigCommerce from '../../assets/bigcommerce.png';
+import Freshbooks from '../../assets/freshbooks.png';
+import Square from '../../assets/square.png';
+import Accordion from 'react-bootstrap/Accordion';
 
 export default function Services() {
   return(
     <div className='services-container'>
-      <h4 className='services-header'>Businesses we serve</h4>
+      <h3 className='services-header'>Businesses we serve</h3>
       <div className='services-first'>
         <div className='services-first-child'>
           <div className='services-first-popular'> Most Popular</div>
@@ -119,16 +126,71 @@ export default function Services() {
           </div>
         </div>
         <div className='services-first-small-child'>
-          <h5 className='services-header'>Clean up services</h5>
+          <h5>Clean up services</h5>
           <p>Request quote</p>
           <p>Book a call for custom pricing and packages</p>
           <Button variant="success" className="book-button">Book</Button>{' '}
         </div>
         <div className='services-first-small-child'>
-          <h5 className='services-header'>Catch up services</h5>
+          <h5>Catch up services</h5>
           <p>Request quote</p>
           <p>Book a call for custom pricing and packages</p>
           <Button variant="success" className="book-button">Book</Button>{' '}
+        </div>
+      </div>
+      <h3 className='services-partner-header'>We partner with the world’s best</h3>
+      <div className='services-second'>
+        <img alt='services' src={BigCommerce} />
+        <img alt='services' src={Stripe} />
+        <img alt='services' src={Freshbooks} />
+        <img alt='services' src={Shopify1} />
+        <img alt='services' src={Gusto1} />
+        <img alt='services' src={Square} />
+      </div>
+      <div className='services-third'>
+        <div className='services-third-a'>
+          <h3>Frequently Asked Questions</h3>
+          <p>More FAQs</p>
+        </div>
+        <div>
+        <Accordion>
+          <Accordion.Item className='services-third-item' eventKey="0">
+            <Accordion.Header>Will Go4Profit file my personal taxes?</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>What type of bookkeeping does Go4Profit offer?</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>How does Go4Profit do my business bookkeeping?</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
         </div>
       </div>
     </div>
